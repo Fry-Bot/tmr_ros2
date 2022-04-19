33bf593796b16905b1b66b2955c8d7dfc70b4612
Jens Vanhooydonck <jensvanhooydonck@gmail.com>
2022-04-19 15:39:46 +0000

    [WIP]: Hybrid Joint trajectory

--- tm_driver/include/tm_driver/tm_ros2_movit_sct.h
+++ tm_driver/include/tm_driver/tm_ros2_movit_sct.h
@@ -1,11 +1,13 @@
 #include "tm_ros2_sct.h"
 #include <trajectory_msgs/msg/joint_trajectory.hpp>
 #include <control_msgs/action/follow_joint_trajectory.hpp>
+#include <control_msgs/action/joint_trajectory.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
 
 class TmRos2SctMoveit : public TmSctRos2{
   private:
     rclcpp_action::Server<control_msgs::action::FollowJointTrajectory>::SharedPtr as_;
+    rclcpp_action::Server<control_msgs::action::JointTrajectory>::SharedPtr asjt_;
     std::mutex as_mtx_;
     std::string goal_id_;
     bool has_goal_;
@@ -22,6 +24,14 @@ class TmRos2SctMoveit : public TmSctRos2{
       std::shared_ptr<rclcpp_action::ServerGoalHandle<control_msgs::action::FollowJointTrajectory>> goal_handle);
     void execute_traj(
       const std::shared_ptr<rclcpp_action::ServerGoalHandle<control_msgs::action::FollowJointTrajectory>> goal_handle);
+    rclcpp_action::GoalResponse handle_goal_joint_trajectory(const rclcpp_action::GoalUUID & uuid,
+      std::shared_ptr<const control_msgs::action::JointTrajectory::Goal> goal);
+    rclcpp_action::CancelResponse handle_cancel_joint_trajectory(
+      const std::shared_ptr<rclcpp_action::ServerGoalHandle<control_msgs::action::JointTrajectory>> goal_handle);
+    void handle_accepted_joint_trajectory(
+      std::shared_ptr<rclcpp_action::ServerGoalHandle<control_msgs::action::JointTrajectory>> goal_handle);
+    void execute_joint_traj(
+      const std::shared_ptr<rclcpp_action::ServerGoalHandle<control_msgs::action::JointTrajectory>> goal_handle);
     void reorder_traj_joints(
       std::vector<trajectory_msgs::msg::JointTrajectoryPoint> &new_traj_points,
       const trajectory_msgs::msg::JointTrajectory& traj);
